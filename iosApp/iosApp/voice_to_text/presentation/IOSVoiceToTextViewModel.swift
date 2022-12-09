9079f1d8586b34324d232f929da14ad874b55048
Philipp Lackner <mail@pl-coding.com>
2022-12-09 21:26:12 +0100

    Implementing IOSVoiceToTextViewModel.swift

---
+++ iosApp/iosApp/voice_to_text/presentation/IOSVoiceToTextViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  IOSVoiceToTextViewModel.swift
+//  iosApp
+//
+//  Created by Philipp Lackner on 09.12.22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+import Combine
+
+@MainActor class IOSVoiceToTextViewModel: ObservableObject {
+    private var parser: any VoiceToTextParser
+    private let languageCode: String
+    
+    private let viewModel: VoiceToTextViewModel
+    @Published var state = VoiceToTextState(powerRatios: [], spokenText: "", canRecord: false, recordError: nil, displayState: nil)
+    private var handle: DisposableHandle?
+    
+    init(parser: VoiceToTextParser, languageCode: String) {
+        self.parser = parser
+        self.languageCode = languageCode
+        self.viewModel = VoiceToTextViewModel(parser: parser, coroutineScope: nil)
+    }
+    
+    func onEvent(event: VoiceToTextEvent) {
+        viewModel.onEvent(event: event)
+    }
+    
+    func startObserving() {
+        handle = viewModel.state.subscribe { [weak self] state in
+            if let state {
+                self?.state = state
+            }
+        }
+    }
+    
+    func dispose() {
+        handle?.dispose()
+        onEvent(event: VoiceToTextEvent.Reset())
+    }
+}
