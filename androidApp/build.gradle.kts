96c1f060a107ffd0942b93a9a499202393c6e208
Kai Cilliers <kaicilliers@protonmail.com>
2025-02-03 12:00:22 +0200

    Upgrade dependencies and target SDK

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -9,11 +9,11 @@ plugins {
 
 android {
     namespace = "com.plcoding.translator_kmm.android"
-    compileSdk = 34
+    compileSdk = 35
     defaultConfig {
         applicationId = "com.plcoding.translator_kmm.android"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
